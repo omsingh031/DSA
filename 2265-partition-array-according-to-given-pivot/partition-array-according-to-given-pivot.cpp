bae2421070326a841385fc4cf4663a1768261884
Om Kumar Singh <160525287+omsingh031@users.noreply.github.com>
2026-06-09 00:27:58 +0530

    Time: 6 ms (66.24%) | Memory: 127.6 MB (88.18%) - LeetSync

--- 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
+++ 2265-partition-array-according-to-given-pivot/partition-array-according-to-given-pivot.cpp
@@ -1,3 +1,4 @@
+
 class Solution {
 public:
     vector<int> pivotArray(vector<int>& nums, int pivot) {
@@ -31,3 +32,15 @@ public:
         return ans;
     }
 };
+
+// Notes:
+// We have solved this question using the index of the beginning of the elements which fall in each individual bracket
+// which is less than pivot, greater than pivot, and equal to pivot: meaning we have divided the ans vector into three parts
+// for stable partitioning
+// First we find the number of elements which are greater than, less than and equal to pivot element and then we decide the indexing;
+// ans={_ _ _ _ _ _ _ _ _ _}
+//      l       e   r
+// l starts from 0, means the index where less than pivot element starts filling which consists of 4 elements
+// r starts from (no.of less + no. of equal to indexing) means the index where more than pivot element starts filling which consists of 4 elements
+// e starts from(no. of less than pivot elements), means the index where the elements equal to pivot starts filling which consists of 2 elements
+
