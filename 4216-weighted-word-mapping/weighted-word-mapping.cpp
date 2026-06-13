0986843cc26bfa55c2355906152d2dfcc59ec246
Om Kumar Singh <160525287+omsingh031@users.noreply.github.com>
2026-06-13 23:49:23 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (95.02%) - LeetSync

---
+++ 4216-weighted-word-mapping/weighted-word-mapping.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string mapWordWeights(vector<string>& words, vector<int>& weights) {
+        string ans;
+
+        for (auto &word : words) {
+            int sum = 0;
+
+            for (char ch : word)
+                sum += weights[ch - 'a'];
+
+            sum %= 26;
+            ans.push_back('z' - sum);
+        }
+
+        return ans;
+    }
+};
