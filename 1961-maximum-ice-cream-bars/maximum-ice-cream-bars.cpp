f439ffb75c346329e27d6a93cd544ffa4ed0aabd
Om Kumar Singh <160525287+omsingh031@users.noreply.github.com>
2026-06-21 19:46:32 +0530

    Time: 26 ms (73.25%) | Memory: 108.6 MB (5.01%) - LeetSync

---
+++ 1961-maximum-ice-cream-bars/maximum-ice-cream-bars.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int maxIceCream(vector<int>& costs, int coins) {
+
+        vector<int> freq(100001, 0);
+
+        for (int cost : costs) {
+            freq[cost]++;
+        }
+
+        int count = 0;
+
+        for (int cost = 1; cost <= 100000; cost++) {
+
+            if (freq[cost] == 0) continue;
+
+            int canBuy = min(freq[cost], coins / cost);
+
+            count += canBuy;
+            coins -= canBuy * cost;
+
+            if (coins < cost) break;
+        }
+
+        return count;
+    }
+};
