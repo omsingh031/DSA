d7fc455ddd28cd242891aac0ad7a685de7831374
Om Kumar Singh <160525287+omsingh031@users.noreply.github.com>
2026-07-25 16:49:44 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (13.40%) - LeetSync

---
+++ 3859-maximum-product-of-two-digits/maximum-product-of-two-digits.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int maxProduct(int n) {
+        string s=to_string(n);
+        sort(s.begin(),s.end());
+        int len=s.size();
+        int p=(s[len-1]-'0')*(s[len-2]-'0');
+        return p;
+        
+    }
+};
